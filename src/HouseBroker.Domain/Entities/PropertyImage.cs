85f9bdd08b728f1521643c64d2e9fcb1efe68299
dikshita11 <52194251+dikshita11@users.noreply.github.com>
2026-05-09 20:23:33 +0545

    feat property entities addition and mapping on domain layer, fix removed reverse project references

---
+++ src/HouseBroker.Domain/Entities/PropertyImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBroker.Domain.Entities
+{
+    public class PropertyImage
+    {
+        public int PropertyId { get; set; }
+        public Property? Property { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ src/HouseBroker.Domain/Entities/PropertyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBroker.Domain.Entities
+{
+    public class PropertyInfo : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PropertyType { get; set; }
+        public string Location { get; set; }
+        public double Area { get; set; }
+        public decimal Price { get; set; }
+        public int RoomCount { get; set; }
+        public string BrokerId { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public PropertyImage Images { get; set; } = new PropertyImage();
+
+    }
+}
