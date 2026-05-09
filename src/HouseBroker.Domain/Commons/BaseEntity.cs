4362815cf1f0751e104a09e381b80c64f1b5655d
dikshita11 <52194251+dikshita11@users.noreply.github.com>
2026-05-09 22:26:18 +0545

    fix: removed automapper and implemented extension methods instead

---
+++ src/HouseBroker.Domain/Commons/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBroker.Domain.Commons
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
