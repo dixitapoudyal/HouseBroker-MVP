da588b7c5247be350f0acf092e4ff125f76e171a
dikshita11 <52194251+dikshita11@users.noreply.github.com>
2026-05-10 18:18:53 +0545

    implemeted interface foe tokengenerating service

---
+++ src/HouseBroker.Infra/Identity/IJwtTokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBroker.Infra.Identity
+{
+    public interface IJwtTokenService
+    {
+        string GenerateToken(ApplicationUser user, IList<string> roles);
+    }
+}
