a68ffed375c4325992dfdb7011b1f2bfb5d322df
dikshita11 <52194251+dikshita11@users.noreply.github.com>
2026-05-10 10:17:34 +0545

    fix: added dependencyinjection in app project that was untracked

---
+++ src/HouseBroker.App/DI.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBroker.App
+{
+    public static class DI
+    {
+        public static IServiceCollection AddApp(
+       this IServiceCollection services)
+        {
+            services.AddScoped<PropertyMapper>();
+
+            // validators and services will be registered here in later phases
+            return services;
+        }
+    }
+}
