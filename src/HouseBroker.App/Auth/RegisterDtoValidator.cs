90f5144165a35914e8873e8dbbe4276dddb9e54c
dikshita11 <52194251+dikshita11@users.noreply.github.com>
2026-05-10 18:06:46 +0545

    made skeletal and rule definition for auth in app project

---
+++ src/HouseBroker.App/Auth/RegisterDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using HouseBroker.App.Auth.D;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBroker.App.Auth
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.FullName).NotEmpty().MaximumLength(100);
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
+
+            RuleFor(x => x.Role)
+                .NotEmpty()
+                .Must(r => r == Roles.Broker || r == Roles.Seeker)
+                .WithMessage($"Role must be either '{Roles.Broker}' or '{Roles.Seeker}'.");
+        }
+    }
+}
