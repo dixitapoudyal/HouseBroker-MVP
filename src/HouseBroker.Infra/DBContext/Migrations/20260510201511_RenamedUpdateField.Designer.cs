3a084daa87521fbd480031ea5a0f8675b30706d7
dikshita11 <52194251+dikshita11@users.noreply.github.com>
2026-05-11 02:12:35 +0545

    adding migrations following field name change

--- src/HouseBroker.Infra/DBContext/Migrations/20260510201511_RenamedUpdateField.Designer.cs
+++ src/HouseBroker.Infra/DBContext/Migrations/20260510201511_RenamedUpdateField.Designer.cs
@@ -4,6 +4,7 @@ using HouseBroker.Infra.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HouseBroker.Infra.DBContext.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260510201511_RenamedUpdateField")]
+    partial class RenamedUpdateField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/HouseBroker.Infra/DBContext/Migrations/20260510201511_RenamedUpdateField.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HouseBroker.Infra.DBContext.Migrations
+{
+    public partial class RenamedUpdateField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ModifiedOn",
+                table: "PropertyInfo",
+                newName: "UpdatedOn");
+
+            migrationBuilder.RenameColumn(
+                name: "ModifiedOn",
+                table: "CommissionPrice",
+                newName: "UpdatedOn");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPhone",
+                table: "PropertyInfo",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactPhone",
+                table: "PropertyInfo");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedOn",
+                table: "PropertyInfo",
+                newName: "ModifiedOn");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedOn",
+                table: "CommissionPrice",
+                newName: "ModifiedOn");
+        }
+    }
+}
