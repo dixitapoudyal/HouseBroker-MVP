f6b3c868557032de57ad0cb67635ff58c503f05e
dikshita11 <52194251+dikshita11@users.noreply.github.com>
2026-05-10 09:55:20 +0545

    feat: added dbcontext and connectionstring placeholder in appsettings

---
+++ src/HouseBroker.Infra/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using HouseBroker.Domain.Entities;
+using HouseBroker.Infra.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBroker.Infra.DBContext
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser> //to get identity tables
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public DbSet<Property> Properties => Set<Property>();
+        public DbSet<PropertyImage> PropertyImages => Set<PropertyImage>();
+        public DbSet<CommissionPrice> CommissionRates => Set<CommissionPrice>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Identity table mapping
+            base.OnModelCreating(builder);
+
+            // pulls all IEntityTypeConfiguration<T> in this assembly
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
