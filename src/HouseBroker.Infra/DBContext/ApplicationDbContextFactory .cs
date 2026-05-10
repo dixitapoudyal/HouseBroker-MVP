6a9e43b57f2824cb3a2fa9d9f9ad6cf4c881cc3f
dikshita11 <52194251+dikshita11@users.noreply.github.com>
2026-05-11 01:58:07 +0545

    feat: implmenting factory class to build the context at design time

---
+++ src/HouseBroker.Infra/DBContext/ApplicationDbContextFactory .cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace HouseBroker.Infra.DBContext;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HouseBrokerDb;Trusted_Connection=True;");
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
